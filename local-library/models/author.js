352ee688ab5d6eecd2f55d5778d281c73c4e9cba
Rishi Raj <rishiraj007bgp@gmail.com>
2024-02-01 16:05:07 +0530

    display book list

--- local-library/models/author.js
+++ local-library/models/author.js
@@ -11,8 +11,8 @@ const AuthorSchema = new Schema({
 
 AuthorSchema.virtual("name").get(function () {
     let fullName = ""
-    if (this.first_name && this.last_name) {
-        fullName = `${this.first_name}, ${this.last_name}`
+    if (this.first_name && this.family_name) {
+        fullName = `${this.first_name}, ${this.family_name}`
     }
     return fullName;
 })
